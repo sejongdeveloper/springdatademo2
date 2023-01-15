c217e98268caede1453c7bc3d5bf26231a7e2b2d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-15 20:29:46 +0900

    JPA 프로그래밍 1.프로젝트 세팅

---
+++ src/main/java/me/whiteship/springdatademo2/Springdatademo2Application.java
@@ -0,0 +1,13 @@
+package me.whiteship.springdatademo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdatademo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springdatademo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/whiteship/springdatademo2/Springdatademo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springdatademo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdatademo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
