91b6eb15ab235265444ef7c464b7a5f1e4618f27
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-16 21:34:09 +0900

    JPA 프로그래밍 7. 쿼리

--- src/main/java/me/whiteship/springdatademo2/JpaRunner.java
+++ src/main/java/me/whiteship/springdatademo2/JpaRunner.java
@@ -1,6 +1,5 @@
 package me.whiteship.springdatademo2;
 
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -8,7 +7,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Component
@@ -19,8 +20,12 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
-        List<Post> posts = query.getResultList();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+        Root<Post> root = query.from(Post.class);
+        query.select(root);
+
+        List<Post> posts = entityManager.createQuery(query).getResultList();
         posts.forEach(System.out::println);
     }
 }
