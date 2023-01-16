dc0a7241caac13250d478e966ace1a60f359d687
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-16 21:39:03 +0900

    스프링 데이터 JPA 원리

---
+++ src/main/java/me/whiteship/springdatademo2/PostRepository.java
@@ -0,0 +1,29 @@
+package me.whiteship.springdatademo2;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class PostRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Post add(Post post) {
+        entityManager.persist(post);
+        return post;
+    }
+
+    public void delete(Post post) {
+        entityManager.remove(post);
+    }
+
+    public List<Post> findAll() {
+        return entityManager.createQuery("select p from Post as p", Post.class)
+                .getResultList();
+    }
+}
