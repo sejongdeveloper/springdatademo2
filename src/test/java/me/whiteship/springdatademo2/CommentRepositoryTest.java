7af1a5661a040ea433daf80f3b2ed504a68a66af
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-17 22:40:52 +0900

    스프링 데이터 Common 3. Null 처리

--- src/test/java/me/whiteship/springdatademo2/CommentRepositoryTest.java
+++ src/test/java/me/whiteship/springdatademo2/CommentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,14 +20,8 @@ public class CommentRepositoryTest {
 
     @Test
     public void crud() {
-        Comment comment = new Comment();
-        comment.setComment("Hello Comment");
-        commentRepository.save(comment);
-
-        List<Comment> all = commentRepository.findAll();
-        assertThat(all.size()).isEqualTo(1);
-
-        long count = commentRepository.count();
-        assertThat(count).isEqualTo(1);
+        Optional<Comment> byId = commentRepository.findById(100L);
+        assertThat(byId).isEmpty();
+        Comment comment = byId.orElse(new Comment());
     }
 }
