5c2c4aae383872236c271bffcf0142339b99344b
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-17 23:55:34 +0900

    스프링 데이터 Common 4. 쿼리 만들기 실습

--- src/main/java/me/whiteship/springdatademo2/CommentRepository.java
+++ src/main/java/me/whiteship/springdatademo2/CommentRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
-    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountAsc(String comment);
+    Page<Comment> findByCommentContainsIgnoreCase(String comment, Pageable pageable);
 
 }
--- src/test/java/me/whiteship/springdatademo2/CommentRepositoryTest.java
+++ src/test/java/me/whiteship/springdatademo2/CommentRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -22,9 +25,11 @@ public class CommentRepositoryTest {
         createCommnet(100, "spring data jpa");
         createCommnet(55, "HIBERNATE SPRING");
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountAsc("Spring");
-        assertThat(comments.size()).isEqualTo(2);
-        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 55);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "likeCount"));
+
+        Page<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+        assertThat(comments.getNumberOfElements()).isEqualTo(2);
+        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 100);
     }
 
     private void createCommnet(int likeCount, String comment) {
