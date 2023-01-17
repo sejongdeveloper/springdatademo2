291fdf88e6d8745b39687507e92ddfc5ba1564e0
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-17 23:49:43 +0900

    스프링 데이터 Common 4. 쿼리 만들기 실습

--- src/main/java/me/whiteship/springdatademo2/CommentRepository.java
+++ src/main/java/me/whiteship/springdatademo2/CommentRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
-    List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String comment, int likeCount);
+    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountAsc(String comment);
 
 }
--- src/test/java/me/whiteship/springdatademo2/CommentRepositoryTest.java
+++ src/test/java/me/whiteship/springdatademo2/CommentRepositoryTest.java
@@ -19,12 +19,18 @@ public class CommentRepositoryTest {
 
     @Test
     public void crud() {
-        Comment comment = new Comment();
-        comment.setLikeCount(100);
-        comment.setComment("spring data jpa");
-        commentRepository.save(comment);
+        createCommnet(100, "spring data jpa");
+        createCommnet(55, "HIBERNATE SPRING");
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseAndLikeCountGreaterThan("Spring", 10);
-        assertThat(comments.size()).isEqualTo(1);
+        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountAsc("Spring");
+        assertThat(comments.size()).isEqualTo(2);
+        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 55);
+    }
+
+    private void createCommnet(int likeCount, String comment) {
+        Comment newComment = new Comment();
+        newComment.setLikeCount(likeCount);
+        newComment.setComment(comment);
+        commentRepository.save(newComment);
     }
 }
